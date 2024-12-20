48cc56c901be1a7adfc52e965d57ed553dae7585
morgul27 <joseminanca2000@gmail.com>
2024-12-20 17:58:40 +0100

    Clase extra para la creacion de datos
    He creado una clase extra para que se almacene la informacion de creacion de datos de la aplicacion

--- app/src/main/java/com/proyecto/MainActivity.kt
+++ app/src/main/java/com/proyecto/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.proyecto.bbdd.UsuarioDatabase
 import com.proyecto.bbdd.entity.Amalgama
@@ -40,6 +41,9 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private lateinit var creacionDatos: CreacionDatos
+
+
     @OptIn(DelicateCoroutinesApi::class)
     @SuppressLint("CoroutineCreationDuringComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -85,19 +89,22 @@ class MainActivity : ComponentActivity() {
                             val viewModel =
                                 MPViewModel(usuRepository, vasRepository, clanRepository)
 
+                            // Inicializo la clase CreacionDatos
+                            creacionDatos = CreacionDatos()
 
                             // Verificar si la tabla está vacía
-                            GlobalScope.launch {
+                            lifecycleScope.launch {
                                 val isEmpty = clanDao.getClan().isEmpty()
                                 if (isEmpty) {
                                     // Insertar los datos iniciales si la tabla está vacía
-                                    insertInitialDataClan(clanDao)
-                                    insertInitialDataDisciplinas(disciplinasClanDao)
-                                    insertNNClanDisciplinas(NNClanDisciplinasDao)
-                                    insertPoderes(poderesDao)
-                                    insertInitialDataAmalgama(amalgamaDao)
+                                    creacionDatos.insertInitialDataClan(clanDao)
+                                    creacionDatos.insertInitialDataDisciplinas(disciplinasClanDao)
+                                    creacionDatos.insertNNClanDisciplinas(NNClanDisciplinasDao)
+                                    creacionDatos.insertPoderes(poderesDao)
+                                    creacionDatos.insertInitialDataAmalgama(amalgamaDao)
                                 }
                             }
+
 
                             Navegacion(viewModel)
 
@@ -116,120 +123,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-// Función para insertar datos iniciales
-private suspend fun insertInitialDataClan(dao: ClanDao) {
-    // Insertar los datos
-    dao.insertClan(Clan(1, nombreClan = "Nosferatu"))
-    dao.insertClan(Clan(2, nombreClan = "Gangrel"))
-    dao.insertClan(Clan(3, nombreClan = "Brujah"))
-
-}
-private suspend fun insertInitialDataDisciplinas(dao: DisciplinasClanDao) {
-    // Nosferatu
-    dao.insertDisciplinasClan(DisciplinasClan(1, nombre = "Animalismo"))
-    dao.insertDisciplinasClan(DisciplinasClan(2, nombre = "Ofuscación"))
-    dao.insertDisciplinasClan(DisciplinasClan(3, nombre = "Potencia"))
-    //Gangrel
-    dao.insertDisciplinasClan(DisciplinasClan(4, nombre = "Fortaleza"))
-    dao.insertDisciplinasClan(DisciplinasClan(5, nombre = "Protean"))
-    //Brujah
-    dao.insertDisciplinasClan(DisciplinasClan(6, nombre = "Celeridad"))
-    dao.insertDisciplinasClan(DisciplinasClan(7, nombre = "Presencia"))
-
-}
-private suspend fun insertNNClanDisciplinas(dao: NNClanDisciplinasDao) {
-    // Nosferatu
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 1, fk_disc = 1)) //Animalismo
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 1, fk_disc = 2)) //Ofuscación
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 1, fk_disc = 3)) //Potencia
-    //Gangrel
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 2, fk_disc = 1)) //Animalismo
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 2, fk_disc = 4)) //Fortaleza
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 2, fk_disc = 5)) //Protean
-    //Brujah
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 3, fk_disc = 3)) //Potencia
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 3, fk_disc = 6)) //Celeridad
-    dao.insertNNClanDisciplinas(NNClanDisciplinas(fk_clan = 3, fk_disc = 7)) //Presencia
-}
-private suspend fun insertPoderes(dao: PoderesDao) {
-    // Animalismo
-    dao.insertPoderes(Poderes(id = 1, nombre = "sentir a la bestia"))
-    dao.insertPoderes(Poderes(id = 2, nombre = "vínculo con famulus"))
-    dao.insertPoderes(Poderes(id = 3, nombre = "susurros salvajes")) //lvl2
-    dao.insertPoderes(Poderes(id = 4, nombre = "colmena no-muerta")) //lvl3
-    dao.insertPoderes(Poderes(id = 5, nombre = "reprimir a la bestia"))
-    dao.insertPoderes(Poderes(id = 6, nombre = "suculencia animal"))
-    dao.insertPoderes(Poderes(id = 7, nombre = "comunión de espíritus"))//lvl4
-    dao.insertPoderes(Poderes(id = 8, nombre = "control animal")) //lvl5
-    dao.insertPoderes(Poderes(id = 9, nombre = "expulsar a la bestia"))
-
-    //Ofuscación
-    dao.insertPoderes(Poderes(id = 10, nombre = "capa de sombras"))
-    dao.insertPoderes(Poderes(id = 11, nombre = "silencio de la muerte"))
-    dao.insertPoderes(Poderes(id = 12, nombre = "paso invisible")) //lvl2
-    dao.insertPoderes(Poderes(id = 13, nombre = "fantasma en la máquina")) //lvl3
-    dao.insertPoderes(Poderes(id = 14, nombre = "máscara de las mil caras"))
-    dao.insertPoderes(Poderes(id = 15, nombre = "desvanecerse")) //lvl4
-    dao.insertPoderes(Poderes(id = 16, nombre = "ocultar"))
-    dao.insertPoderes(Poderes(id = 17, nombre = "disfraz del impostor")) //lvl5
-    dao.insertPoderes(Poderes(id = 18, nombre = "encubrimiento de la concurrencia"))
-
-    //Potencia
-    dao.insertPoderes(Poderes(id = 19, nombre = "cuerpo letal"))
-    dao.insertPoderes(Poderes(id = 20, nombre = "salto vertiginoso"))
-    dao.insertPoderes(Poderes(id = 21, nombre = "bravura")) //lvl2
-    dao.insertPoderes(Poderes(id = 22, nombre = "agarre asombroso")) //lvl3
-    dao.insertPoderes(Poderes(id = 23, nombre = "alimentación brutal"))
-    dao.insertPoderes(Poderes(id = 24, nombre = "chispa de ira"))
-    dao.insertPoderes(Poderes(id = 25, nombre = "sorbo de poderío")) //lvl4
-    dao.insertPoderes(Poderes(id = 26, nombre = "puño de caín")) //lvl5
-    dao.insertPoderes(Poderes(id = 27, nombre = "temblor de tierra"))
-
-    //Fortaleza
-    dao.insertPoderes(Poderes(id = 28, nombre = "mente imperturbable"))
-    dao.insertPoderes(Poderes(id = 29, nombre = "resiliencia"))
-    dao.insertPoderes(Poderes(id = 30, nombre = "bestias resistentes")) //lvl2
-    dao.insertPoderes(Poderes(id = 31, nombre = "dureza"))
-    dao.insertPoderes(Poderes(id = 32, nombre = "desafiar prohibición")) //lvl3
-    dao.insertPoderes(Poderes(id = 33, nombre = "fortificar la fachada interior"))
-    dao.insertPoderes(Poderes(id = 34, nombre = "sorbo de aguante")) //lvl4
-    dao.insertPoderes(Poderes(id = 35, nombre = "arrojo por el dolor")) //lvl5
-    dao.insertPoderes(Poderes(id = 36, nombre = "carne de mármol"))
-
-    //Protean
-    dao.insertPoderes(Poderes(id = 37, nombre = "ojos de la bestia"))
-    dao.insertPoderes(Poderes(id = 38, nombre = "peso de la pluma"))
-    dao.insertPoderes(Poderes(id = 39, nombre = "armas salvajes")) //lvl2
-    dao.insertPoderes(Poderes(id = 40, nombre = "cambiar de forma")) //lvl3
-    dao.insertPoderes(Poderes(id = 41, nombre = "fusión con la tierra"))
-    dao.insertPoderes(Poderes(id = 42, nombre = "metamorfosis")) //lvl4
-    dao.insertPoderes(Poderes(id = 43, nombre = "corazón liberado")) //lvl5
-    dao.insertPoderes(Poderes(id = 44, nombre = "forma de niebla"))
-
-    //Celeridad
-    dao.insertPoderes(Poderes(id = 45, nombre = "gracia felina"))
-    dao.insertPoderes(Poderes(id = 46, nombre = "reflejos rápidos"))
-    dao.insertPoderes(Poderes(id = 47, nombre = "presteza")) //lvl2
-    dao.insertPoderes(Poderes(id = 48, nombre = "pestañeo")) //lvl3
-    dao.insertPoderes(Poderes(id = 49, nombre = "recorrido"))
-    dao.insertPoderes(Poderes(id = 50, nombre = "puntería certera")) //lvl4
-    dao.insertPoderes(Poderes(id = 51, nombre = "sorbo de elegancia"))
-    dao.insertPoderes(Poderes(id = 52, nombre = "golpe relámpago")) //lvl5
-    dao.insertPoderes(Poderes(id = 53, nombre = "segundo quebrado"))
-
-    //Presencia
-    dao.insertPoderes(Poderes(id = 54, nombre = "atemorizar"))
-    dao.insertPoderes(Poderes(id = 55, nombre = "fascinación"))
-    dao.insertPoderes(Poderes(id = 56, nombre = "beso persistente")) //lvl2
-    dao.insertPoderes(Poderes(id = 57, nombre = "encantamiento")) //lvl3
-    dao.insertPoderes(Poderes(id = 58, nombre = "mirada aterradora"))
-    dao.insertPoderes(Poderes(id = 59, nombre = "invocación")) //lvl4
-    dao.insertPoderes(Poderes(id = 60, nombre = "voz irresistible"))
-    dao.insertPoderes(Poderes(id = 61, nombre = "magnetismo de estrella")) //lvl5
-    dao.insertPoderes(Poderes(id = 62, nombre = "majestad"))
-
-}
 
 private suspend fun insertInitialDataAmalgama(dao: AmalgamaDao) {
     //Animalismo
